751f459533d20952727209922452051a8dfd1952
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-21 15:48:35 -0500

    Updated Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,12 +1,15 @@
-function Header() {
+export function Header() {
   return (
     <>
-      <header>
-        <h1 className="">Hello,</h1>
-        <h1>World!</h1>
+      <header className="flex h-screen bg-stone-900">
+        <div className="select-none pl-96 pt-36 font-mono text-[80px] font-extrabold">
+          <h1>Hello,</h1>
+          <h1 className="glow-white">World!</h1>
+        </div>
+        <h1 className="glow pb-9 font-mono text-[45px] text-sky-700 underline">
+          aaron-cordova-xyz
+        </h1>
       </header>
     </>
-  )
+  );
 }
-
-export default Header
