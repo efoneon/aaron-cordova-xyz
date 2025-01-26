a9d2e377a312b93a3ba3240bcc3ce77aee31cf54
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-26 17:16:40 -0500

    Imported message into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
+import {loadedMessage} from './console-messages/message';
 import App from './App';
 import './index.css';
+
+loadedMessage();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
