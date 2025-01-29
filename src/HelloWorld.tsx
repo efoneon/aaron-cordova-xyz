85d779f4977ea85e42343bfcf4723811abde9ab5
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-29 15:21:37 -0500

    Changed color of text "Hello"

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -2,11 +2,13 @@ export function HelloWorld() {
   return (
     <>
       <div className="h-screen bg-stone-900 pl-96 pt-36 font-mono text-[80px] font-extrabold">
-        <h1>Hello,</h1>
+        <h1 className="text-[#a6a6a6]">Hello,</h1>
         <h1 className="glow-earth bg-gradient-to-r from-blue-700 to-green-700 bg-clip-text text-transparent">
           World.
         </h1>
       </div>
+
+      {/* GRADIENT: Fade into black */}
       <div className="h-60 bg-gradient-to-b from-stone-900 to-black"></div>
     </>
   );
