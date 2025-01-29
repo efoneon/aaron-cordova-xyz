bc17862f5c8fe9a6813eceb3c8a0258128123ab8
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-29 15:19:18 -0500

    Added Arrow component for all the current links in Header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,16 +1,33 @@
+import {Arrow} from './Arrow';
+
 export function Header() {
   return (
-    <header className="sticky top-0 flex border-b-2 bg-black p-4">
+    <header className="sticky top-0 flex border-b-2 bg-[#111111] p-4">
       <h1
-        className="glow cursor-pointer font-mono text-[45px] text-sky-700 underline"
+        className="glow cursor-pointer font-mono text-5xl text-sky-700 underline"
         onClick={refreshToHeader}
       >
         aaron-cordova-xyz
       </h1>
-      <div className="flex gap-8 pl-10 pt-6 text-xl text-white">
-        <a href="./">Explore</a>
-        <a href="#about">About</a>
-        <a>hello again</a>
+      <div className="flex gap-8 pl-10 pt-3 text-xl text-white">
+        <div className="flex gap-1">
+          <a href="./">Explore</a>
+          <div className="translate-y-1">
+            <Arrow />
+          </div>
+        </div>
+        <div className="flex gap-1">
+          <a href="./about.tsx">About</a>
+          <div className="translate-y-1">
+            <Arrow />
+          </div>
+        </div>
+        <div className="flex gap-1">
+          <a href="./about.tsx">hello again</a>
+          <div className="translate-y-1">
+            <Arrow />
+          </div>
+        </div>
       </div>
     </header>
   );
