9d0108c63277faa4186bb8af30caa2efc91b30ce
Aaron <115902510+efoneon@users.noreply.github.com>
2025-01-29 15:23:21 -0500

    Added a waving hand emoji for console message

--- src/console-messages/message.ts
+++ src/console-messages/message.ts
@@ -1,5 +1,5 @@
 export function loadedMessage() {
   document.addEventListener('DOMContentLoaded', () => {
-    console.log('Hey nerd, welcome to the XYZ console!');
+    console.log('👋 Hey nerd, welcome to the XYZ console!');
   });
 }
